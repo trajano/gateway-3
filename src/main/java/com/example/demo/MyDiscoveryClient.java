6b101cc2a1ccd4e1ec5d62b8a5c9b81fe91bbb57
Archimedes Trajano <archimedes.trajano@devhaus.com>
2021-02-15 20:23:28 -0500

    Used ReactiveDiscoveryClient

--- src/main/java/com/example/demo/MyDiscoveryClient.java
+++ src/main/java/com/example/demo/MyDiscoveryClient.java
@@ -1,32 +1,44 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Component
-public class MyDiscoveryClient implements DiscoveryClient {
+public class MyDiscoveryClient implements ReactiveDiscoveryClient {
+    private final DiscoveryLocatorProperties discoveryLocatorProperties;
+
+    public MyDiscoveryClient(final DiscoveryLocatorProperties discoveryLocatorProperties) {
+        this.discoveryLocatorProperties = discoveryLocatorProperties;
+    }
+
     @PostConstruct
     public void hello() {
-        System.out.println("Hello world");
+        System.out.println("Hello world" + discoveryLocatorProperties);
     }
+
     @Override
     public String description() {
         return "My Discovery Client";
     }
 
     @Override
-    public List<ServiceInstance> getInstances(String serviceId) {
-        System.out.println(serviceId);
-        return List.of();
+    public Flux<ServiceInstance> getInstances(String serviceId) {
+        System.out.println("getInstances()" + serviceId);
+        return Flux.just();
     }
 
     @Override
-    public List<String> getServices() {
-        System.out.println("getServices");
-        return List.of();
+    public Flux<String> getServices() {
+        System.out.println("getServices()");
+        return Flux.just();
     }
+
 }
